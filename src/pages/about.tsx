4fa44a79c99b58470f259d5d605082cbe3cd6721
Igor <80823011+igorwastaken@users.noreply.github.com>
2024-04-07 15:22:04 +0000

    t

---
+++ src/pages/about.tsx
@@ -0,0 +1,9 @@
+const About = function () {
+    return (
+        <>
+        <p>Test</p>
+        </>
+    )
+}
+
+export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,11 +18,11 @@ const home = function Home(_props: InferGetStaticPropsType<typeof getStaticProps
         <Image width="150" height="150" className="rounded-full" src={"/igor.png"} alt="Yeah, that's me."></Image>
         <div className="flex flex-col mt-1 md:mt-0 md:ml-4">
           <p className="text-xl font-black">{t("hello")}</p>
-          <p>Yeah, that's me, my mom said that am a beautiful boy.</p>
+          <p>{t("mymom")}</p>
         </div>
       </div>
       <div className="md:p-5 w-full flex flex-col md:flex-row justify-evenly items-center text-center">
-        <button className="mt-1 text-center bg-blue-700 text-blue-200 p-3 rounded-xl">Who I am?</button>
+        <button className="mt-1 text-center bg-blue-700 text-blue-200 p-3 rounded-xl" onClick={() => router.push("/about")}>{t("about")}</button>
         <button className="mt-3 text-center bg-green-700 text-green-200 p-3 rounded-xl" >Spotify</button>
       </div>
     </div>
