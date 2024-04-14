975e8becd3a79b339df532a0cbe2a48ee3509aad
Igor <80823011+igorwastaken@users.noreply.github.com>
2024-04-14 03:59:18 +0000

    s

--- src/pages/api/putSong.tsx
+++ src/pages/api/putSong.tsx
@@ -7,11 +7,11 @@ export const runtime = 'edge'
 export default async function PutSong(req: NextApiRequest, res: NextApiResponse) {
     if(req.method?.toLowerCase() !== "post") return NextResponse.json({ code: 401, text: "Unauthorized" }, { status: 401 });
     const url = new URL(req.url as string)
-    const song = await putSong(url.searchParams.get("song") as string)
+    /*const song = await putSong(url.searchParams.get("song") as string)
     if(song.status !== 200) return NextResponse.json({
         code: 500,
         text: 'Internal Server Error.'
-    }, { status: 500 });
+    }, { status: 500 });*/
     return NextResponse.json({
         code: 200,
         text: "success!"
