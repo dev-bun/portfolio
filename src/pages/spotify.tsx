499dfde0eed42900619d57f608d919ca1798f207
Igor <80823011+igorwastaken@users.noreply.github.com>
2024-04-14 21:22:17 -0300

    Update spotify.tsx

--- src/pages/spotify.tsx
+++ src/pages/spotify.tsx
@@ -28,8 +28,8 @@ export default function Spotify() {
       }
       getCol()
     }, [spotify])
-    return(<Layout className="#1b1b1b">
-        <div className="flex flex-col w-full h-[94vh] md:h-screen bg-[#1b1b1b]">
+    return(<Layout>
+        <div className="flex flex-col w-full h-[94vh] md:h-screen">
             <div className={spotify?.isPlaying?`p-5 w-full fixed z-[50] blur-3xl transition-all`:"p-5 w-full fixed z-[50] blur-3xl transition-all bg-[#1b1b1b]"} style={{ backgroundColor: `${color}` }}></div>
             <div className="p-4 sticky z-[60]"><Image width="130" height="130" alt="Spotify logo" src={"/Spotify_Logo_RGB_White.png"}/></div>
             {spotify?.isPlaying?(
