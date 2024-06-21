c4cdf1494467fa80bf1be6de4f5a4d952625d830
Igor <80823011+igorwastaken@users.noreply.github.com>
2024-06-21 12:28:58 -0300

    Update spotify.tsx

--- src/pages/api/spotify.tsx
+++ src/pages/api/spotify.tsx
@@ -155,7 +155,7 @@ export default async function GET(req: NextApiRequest, res: NextApiResponse<Song
  // console.log(history)
   var lasthour = new Date();
   lasthour.setHours(lasthour.getHours()-1)
-  await history.filter((q: any) => { return new Date(q.played_at).getTime() >= lasthour.getTime();}).forEach(async (q: any) => {
+  await history.forEach(async (q: any) => {
     // console.log(q.track.album.images)
     if (history.length === 0) return;
     historyItem.push({
